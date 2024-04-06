e11a4db77cc7e3c2260de27c241fc5a40ff6a13e
otaeho <dhxogh123123@naver.com>
2024-04-06 23:16:16 +0900

    feat(AnswerRepository): 퀴즈에 맞는 Answer 엔티티 찾는 기능 작성

--- JookBiRen/src/main/java/com/ablez/jookbiren/answer/repository/AnswerRepository.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/answer/repository/AnswerRepository.java
@@ -1,2 +1,37 @@
-package com.ablez.jookbiren.answer.repository;public class AnswerRepository {
+package com.ablez.jookbiren.answer.repository;
+
+import static com.ablez.jookbiren.answer.entity.QAnswerEp01.answerEp01;
+import static com.ablez.jookbiren.quiz.entity.QQuizEp01.quizEp01;
+
+import com.ablez.jookbiren.answer.entity.AnswerEp01;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class AnswerRepository {
+    private final EntityManager em;
+    private JPAQueryFactory queryFactory;
+
+    public AnswerRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(this.em);
+    }
+
+    public Optional<AnswerEp01> findByQuiz(int placeCode, int quizNumber) {
+        AnswerEp01 answer = queryFactory
+                .select(answerEp01)
+                .from(answerEp01)
+                .join(answerEp01.quizEp01, quizEp01)
+                .where(
+                        quizEp01.placeCode.eq(placeCode),
+                        quizEp01.quizNumber.eq(quizNumber)
+                )
+                .fetchOne();
+
+        return Optional.ofNullable(answer);
+    }
 }
