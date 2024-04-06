574d3fbcd7f31d124edfb2be38f8cecc89145bd3
otaeho <dhxogh123123@naver.com>
2024-04-06 20:56:37 +0900

    chore(QuizEp01): 문제 관련 엔티티 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/entity/QuizEp01.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/entity/QuizEp01.java
@@ -1,2 +1,22 @@
-package com.ablez.jookbiren.quiz.entity;public class QuizEp01 {
+package com.ablez.jookbiren.quiz.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class QuizEp01 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long quizId;
+    @Column(nullable = false)
+    private int placeCode;
+    @Column(nullable = false)
+    private int quizNumber;
 }
