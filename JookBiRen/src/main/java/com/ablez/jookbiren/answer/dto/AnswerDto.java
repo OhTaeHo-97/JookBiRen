291323733607c8eb5f110d4ecaa3eb84decdbb5a
otaeho <dhxogh123123@naver.com>
2024-04-06 23:12:02 +0900

    chore(AnswerDto): 정답 보기 요청, 응답 DTO 작성

--- JookBiRen/src/main/java/com/ablez/jookbiren/answer/dto/AnswerDto.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/answer/dto/AnswerDto.java
@@ -1,2 +1,26 @@
-package com.ablez.jookbiren.answer.dto;public class AnswerDto {
+package com.ablez.jookbiren.answer.dto;
+
+import lombok.Getter;
+
+public class AnswerDto {
+
+    @Getter
+    public static class FindAnswerDto {
+        private int placeCode;
+        private int quizNumber;
+
+        public FindAnswerDto(String quizInfo) {
+            placeCode = quizInfo.charAt(0) - '0';
+            quizNumber = Integer.parseInt(quizInfo.substring(1));
+        }
+    }
+
+    @Getter
+    public static class FindAnswerResponseDto {
+        private String answer = "";
+
+        public FindAnswerResponseDto(String answer) {
+            this.answer = answer;
+        }
+    }
 }
