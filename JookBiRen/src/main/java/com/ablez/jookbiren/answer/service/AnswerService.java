0cf0faa848aa9805e3d507a44880e50dbfb6349d
otaeho <dhxogh123123@naver.com>
2024-04-06 23:20:18 +0900

    feat(AnswerService): 정답 보기 비즈니스 메서드 작성

--- JookBiRen/src/main/java/com/ablez/jookbiren/answer/service/AnswerService.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/answer/service/AnswerService.java
@@ -1,2 +1,26 @@
-package com.ablez.jookbiren.answer.service;public class AnswerService {
+package com.ablez.jookbiren.answer.service;
+
+import com.ablez.jookbiren.answer.dto.AnswerDto;
+import com.ablez.jookbiren.answer.dto.AnswerDto.FindAnswerDto;
+import com.ablez.jookbiren.answer.dto.AnswerDto.FindAnswerResponseDto;
+import com.ablez.jookbiren.answer.entity.AnswerEp01;
+import com.ablez.jookbiren.answer.repository.AnswerRepository;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public AnswerDto.FindAnswerResponseDto findAnswer(FindAnswerDto quizInfo) {
+        Optional<AnswerEp01> optionalAnswer = answerRepository.findByQuiz(quizInfo.getPlaceCode(),
+                quizInfo.getQuizNumber());
+        AnswerEp01 answer = optionalAnswer.orElseThrow(() -> new NoSuchElementException("해당 문제가 존재하지 않습니다."));
+        return new FindAnswerResponseDto(answer.getAnswer());
+    }
 }
