feb79d24b99225a0e1f753d3258ea01eaf56cd54
otaeho <dhxogh123123@naver.com>
2024-04-10 17:33:36 +0900

    feat(QuizService): 현재 상황 표시를 위한 비즈니스 메서드 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/service/QuizService.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/service/QuizService.java
@@ -1,2 +1,31 @@
-package com.ablez.jookbiren.quiz.service;public class QuizService {
+package com.ablez.jookbiren.quiz.service;
+
+import com.ablez.jookbiren.quiz.dto.QuizDto.PageDto;
+import com.ablez.jookbiren.quiz.entity.Quiz0Ep01;
+import com.ablez.jookbiren.quiz.repository.QuizRepository;
+import com.ablez.jookbiren.user.entity.UserEp01;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class QuizService {
+    private final QuizRepository quizRepository;
+
+    public PageDto getCurrentSituationAndSolvedProblems(UserEp01 user) {
+        // UserEp01 user = userService.findUser(코드);
+        user.getSolvedQuizCount();
+        user.getAnswerCount();
+        List<Quiz0Ep01> solvedQuizzes = quizRepository.findAllQuiz0(user);
+
+        return new PageDto(user.getSolvedQuizCount(), user.getAnswerCount(), quizzesToQuizNumbers(solvedQuizzes));
+    }
+
+    private List<Integer> quizzesToQuizNumbers(List<Quiz0Ep01> quizzes) {
+        return quizzes.stream().map(quiz -> quiz.getQuizNumber()).collect(Collectors.toList());
+    }
 }
