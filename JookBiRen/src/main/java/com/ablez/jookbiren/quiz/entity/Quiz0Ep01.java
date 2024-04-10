15d41409e2cfddde98b542a24ea519e411bdd723
otaeho <dhxogh123123@naver.com>
2024-04-10 17:30:03 +0900

    chore(Quiz0Ep01): 사용자가 푼 안국역 문제를 나타내는 엔티티 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/entity/Quiz0Ep01.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/entity/Quiz0Ep01.java
@@ -1,2 +1,30 @@
-package com.ablez.jookbiren.quiz.entity;public class Quiz0Ep01 {
+package com.ablez.jookbiren.quiz.entity;
+
+import com.ablez.jookbiren.user.entity.UserEp01;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Quiz0Ep01 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long quiz0Id;
+    private Integer quizNumber;
+    private LocalDateTime firstAccessTime;
+    private LocalDateTime firstAnswerTie;
+    private LocalDateTime getHintTime;
+    private LocalDateTime getAnswerTime;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserEp01 userId;
 }
