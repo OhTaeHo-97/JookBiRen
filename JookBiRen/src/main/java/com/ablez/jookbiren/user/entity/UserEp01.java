bcf0c0107941b53636ac5483a09cafb5aac6b8d2
otaeho <dhxogh123123@naver.com>
2024-04-10 17:34:04 +0900

    chore(UserEp01): 사용자 엔티티 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/user/entity/UserEp01.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/user/entity/UserEp01.java
@@ -1,2 +1,33 @@
-package com.ablez.jookbiren.user.entity;public class UserEp01 {
+package com.ablez.jookbiren.user.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class UserEp01 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @Column(nullable = false)
+    private String code;
+    private Integer score = 0;
+    private Integer answerStatusCode = 0;
+    private Integer answerCount = 0;
+    private Integer solvedQuizCount = 0;
+    private Integer criminal = 0;
+    private LocalDateTime answerTime;
+    private LocalDateTime firstLoginTime;
+
+    public UserEp01(Long userId, String code) {
+        this.userId = userId;
+        this.code = code;
+    }
 }
