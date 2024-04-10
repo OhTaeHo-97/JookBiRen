d21c7156e301c88a45efb431b61f1ddc71c5752e
otaeho <dhxogh123123@naver.com>
2024-04-10 17:26:36 +0900

    feat(QuizController): 현재 상황(정답 본 횟수, 푼 문제 개수, 푼 문제) 표시를 위한 핸들러 메서드 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/controller/QuizController.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/controller/QuizController.java
@@ -1,2 +1,23 @@
-package com.ablez.jookbiren.quiz.controller;public class QuizController {
+package com.ablez.jookbiren.quiz.controller;
+
+import com.ablez.jookbiren.quiz.service.QuizService;
+import com.ablez.jookbiren.user.entity.UserEp01;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/quizzes")
+public class QuizController {
+    private final QuizService quizService;
+
+    @GetMapping
+    public ResponseEntity getCurrentSituationAndSolvedProblems() {
+        return new ResponseEntity(quizService.getCurrentSituationAndSolvedProblems(new UserEp01(1L, "abc")),
+                HttpStatus.OK);
+    }
 }
---
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/dto/QuizDto.java
@@ -0,0 +1,2 @@
+package com.ablez.jookbiren.quiz.dto;public class QuizDto {
+}
