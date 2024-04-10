42597c9cf0c332d8d2c44b56b1b18ed94ed74f1f
otaeho <dhxogh123123@naver.com>
2024-04-10 17:40:31 +0900

    chore(Quiz1Ep01): 사용자가 푼 창덕궁 문제를 나타내는 엔티티 생 성

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/entity/Quiz1Ep01.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/entity/Quiz1Ep01.java
@@ -1,2 +1,30 @@
-package com.ablez.jookbiren.quiz.entity;public class Quiz1Ep01 {
+package com.ablez.jookbiren.quiz.entity;
+
+import com.ablez.jookbiren.user.entity.UserEp01;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Quiz1Ep01 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long quiz1Id;
+    private Integer quizNumber;
+    private LocalDateTime firstAccessTime;
+    private LocalDateTime firstAnswerTime;
+    private LocalDateTime getHintTime;
+    private LocalDateTime getAnswerTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEp01 userId;
 }
