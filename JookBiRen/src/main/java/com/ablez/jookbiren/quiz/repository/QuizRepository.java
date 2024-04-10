d3131908d621df067f1190d4ac2a497c850eced7
otaeho <dhxogh123123@naver.com>
2024-04-10 17:32:42 +0900

    feat(QuizRepository): 사용자가 푼 안국역 문제들을 가져오는 메서드 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/repository/QuizRepository.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/repository/QuizRepository.java
@@ -1,2 +1,30 @@
-package com.ablez.jookbiren.quiz.repository;public class QuizRepositoryt {
+package com.ablez.jookbiren.quiz.repository;
+
+import static com.ablez.jookbiren.quiz.entity.QQuiz0Ep01.quiz0Ep01;
+
+import com.ablez.jookbiren.quiz.entity.Quiz0Ep01;
+import com.ablez.jookbiren.user.entity.UserEp01;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class QuizRepository {
+    private final EntityManager em;
+    private JPAQueryFactory queryFactory;
+
+    public QuizRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(this.em);
+    }
+
+    public List<Quiz0Ep01> findAllQuiz0(UserEp01 user) {
+        return queryFactory
+                .selectFrom(quiz0Ep01)
+                .where(quiz0Ep01.userId.eq(user))
+                .fetch();
+    }
 }
