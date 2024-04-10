ab9d6ec452ef868b94e18e4bd9ef985d4c5c135f
otaeho <dhxogh123123@naver.com>
2024-04-10 17:29:05 +0900

    chore(QuizDto): 현재 상황을 나타내는 응답 DTO 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/dto/QuizDto.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/dto/QuizDto.java
@@ -1,2 +1,19 @@
-package com.ablez.jookbiren.quiz.dto;public class QuizDto {
+package com.ablez.jookbiren.quiz.dto;
+
+import java.util.List;
+import lombok.Getter;
+
+public class QuizDto {
+    @Getter
+    public static class PageDto {
+        private int solved;
+        private int answer;
+        private List<Integer> solvedProblems;
+
+        public PageDto(int solved, int answer, List<Integer> solvedProblems) {
+            this.solved = solved;
+            this.answer = answer;
+            this.solvedProblems = solvedProblems;
+        }
+    }
 }
