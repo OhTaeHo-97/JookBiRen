054be8d75a307ea1ce2cdc44ac15541b9a647d65
otaeho <dhxogh123123@naver.com>
2024-04-14 21:22:33 +0900

    chore(JookBiRenDto): 문제 정보 DTO 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/dto/JookBiRenDto.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/dto/JookBiRenDto.java
@@ -1,2 +1,21 @@
-package com.ablez.jookbiren.dto;public class JookBiRenDto {
+package com.ablez.jookbiren.dto;
+
+import lombok.Getter;
+
+public class JookBiRenDto {
+    @Getter
+    public static class Quiz {
+        private int placeCode;
+        private int quizNumber;
+
+        public Quiz(String quizInfo) {
+            placeCode = quizInfo.charAt(0) - '0';
+            quizNumber = Integer.parseInt(quizInfo.substring(1));
+        }
+
+        public Quiz(int placeCode, int quizNumber) {
+            this.placeCode = placeCode;
+            this.quizNumber = quizNumber;
+        }
+    }
 }
