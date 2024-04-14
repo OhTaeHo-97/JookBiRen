dec23d5f3d6cdf7a8547f7f0f355ae2551bea4cc
otaeho <dhxogh123123@naver.com>
2024-04-14 21:23:39 +0900

    refactor(AnswerDto): 문제 관련 DTO(FindAnswerDto) 삭제

--- JookBiRen/src/main/java/com/ablez/jookbiren/answer/dto/AnswerDto.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/answer/dto/AnswerDto.java
@@ -1,20 +1,9 @@
 package com.ablez.jookbiren.answer.dto;
 
+import com.ablez.jookbiren.dto.JookBiRenDto.Quiz;
 import lombok.Getter;
 
 public class AnswerDto {
-
-    @Getter
-    public static class FindAnswerDto {
-        private int placeCode;
-        private int quizNumber;
-
-        public FindAnswerDto(String quizInfo) {
-            placeCode = quizInfo.charAt(0) - '0';
-            quizNumber = Integer.parseInt(quizInfo.substring(1));
-        }
-    }
-
     @Getter
     public static class FindAnswerResponseDto {
         private String answer = "";
@@ -26,11 +15,11 @@ public class AnswerDto {
 
     @Getter
     public static class CheckAnswerDto {
-        private FindAnswerDto quizInfo;
+        private Quiz quizInfo;
         private String answer;
 
         public CheckAnswerDto(String quiz, String answer) {
-            this.quizInfo = new FindAnswerDto(quiz);
+            this.quizInfo = new Quiz(quiz);
             this.answer = answer;
         }
     }
