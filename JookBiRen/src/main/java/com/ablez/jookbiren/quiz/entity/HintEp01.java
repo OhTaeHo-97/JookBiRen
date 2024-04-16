0b1dbd5f170a0daa1932145d6567545fb896085b
otaeho <dhxogh123123@naver.com>
2024-04-16 23:23:59 +0900

    chore(HintEp01): 힌트 엔티티 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/entity/HintEp01.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/entity/HintEp01.java
@@ -1,2 +1,26 @@
-package com.ablez.jookbiren.quiz.entity;public class HintEp01 {
+package com.ablez.jookbiren.quiz.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class HintEp01 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long hintId;
+    @Column(nullable = false)
+    private String hint;
+
+    @OneToOne
+    @JoinColumn(name = "quiz_id")
+    private QuizEp01 quiz;
 }
