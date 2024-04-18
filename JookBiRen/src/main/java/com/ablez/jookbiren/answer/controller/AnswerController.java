58c3266e29e115b8dfa8bcb17a189b964243a2d2
otaeho <dhxogh123123@naver.com>
2024-04-18 21:37:39 +0900

    feat(AnswerController): 범인 지목 핸들러 메서드 작성

--- JookBiRen/src/main/java/com/ablez/jookbiren/answer/controller/AnswerController.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/answer/controller/AnswerController.java
@@ -1,12 +1,16 @@
 package com.ablez.jookbiren.answer.controller;
 
 import com.ablez.jookbiren.answer.dto.AnswerDto.CheckAnswerDto;
+import com.ablez.jookbiren.answer.dto.AnswerDto.SuspectDto;
 import com.ablez.jookbiren.answer.service.AnswerService;
 import com.ablez.jookbiren.dto.JookBiRenDto.Quiz;
+import com.ablez.jookbiren.user.entity.UserEp01;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +28,10 @@ public class AnswerController {
     @GetMapping("/quiz")
     public ResponseEntity checkAnswer(CheckAnswerDto dto) {
         return new ResponseEntity<>(answerService.checkAnswer(dto), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity pickSuspect(@RequestBody SuspectDto suspectInfo) {
+        return new ResponseEntity(answerService.pickSuspect(new UserEp01(1L, "abc"), suspectInfo), HttpStatus.OK);
     }
 }
