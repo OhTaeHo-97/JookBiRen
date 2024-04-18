1d5c9b7024fb1d43ef5948903c5317e3ccb5bdfc
otaeho <dhxogh123123@naver.com>
2024-04-18 21:38:50 +0900

    feat(AnswerService): 범인 지목 비즈니스 메서드 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/answer/service/AnswerService.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/answer/service/AnswerService.java
@@ -1,12 +1,16 @@
 package com.ablez.jookbiren.answer.service;
 
+import com.ablez.jookbiren.answer.constant.AnswerConstant;
 import com.ablez.jookbiren.answer.dto.AnswerDto;
 import com.ablez.jookbiren.answer.dto.AnswerDto.CheckAnswerDto;
 import com.ablez.jookbiren.answer.dto.AnswerDto.CheckAnswerResponseDto;
 import com.ablez.jookbiren.answer.dto.AnswerDto.FindAnswerResponseDto;
+import com.ablez.jookbiren.answer.dto.AnswerDto.SuspectDto;
+import com.ablez.jookbiren.answer.dto.AnswerDto.SuspectResponseDto;
 import com.ablez.jookbiren.answer.entity.AnswerEp01;
 import com.ablez.jookbiren.answer.repository.AnswerRepository;
 import com.ablez.jookbiren.dto.JookBiRenDto.Quiz;
+import com.ablez.jookbiren.user.entity.UserEp01;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import javax.transaction.Transactional;
@@ -31,5 +35,14 @@ public class AnswerService {
                 answerInfo.getQuizInfo().getPlaceCode(),
                 answerInfo.getQuizInfo().getQuizNumber(), answerInfo.getAnswer());
         return new CheckAnswerResponseDto(optionalAnswer.isPresent());
+    }
+
+    public SuspectResponseDto pickSuspect(UserEp01 user, SuspectDto suspectInfo) {
+        if (user.getCriminal() == 0) {
+            user.updateCriminal(suspectInfo.getSuspect());
+            return new SuspectResponseDto();
+        } else {
+            return new SuspectResponseDto(AnswerConstant.SUSPECT.get(user.getCriminal()));
+        }
     }
 }
