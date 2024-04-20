e0d2a40246eab44246a476f918d7eef3657be309
otaeho <dhxogh123123@naver.com>
2024-04-20 16:42:41 +0900

    chore(ExceptionCode): 자체 예외 코드 생성

---
+++ JookBiRen/src/main/java/com/ablez/jookbiren/exception/BusinessLogicException.java
@@ -0,0 +1,2 @@
+package com.ablez.jookbiren.exception;public class BusinessLogicException {
+}
---
+++ JookBiRen/src/main/java/com/ablez/jookbiren/exception/ExceptionCode.java
@@ -0,0 +1,17 @@
+package com.ablez.jookbiren.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    ;
+
+    @Getter
+    private int status;
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
