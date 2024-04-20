e519ea8e7ac88a88eb0dbacdc8cffdac0d39345a
otaeho <dhxogh123123@naver.com>
2024-04-20 16:44:01 +0900

    chore(BusinessLogicException): 자체 예외 생성

--- JookBiRen/src/main/java/com/ablez/jookbiren/exception/BusinessLogicException.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/exception/BusinessLogicException.java
@@ -1,2 +1,13 @@
-package com.ablez.jookbiren.exception;public class BusinessLogicException {
+package com.ablez.jookbiren.exception;
+
+import lombok.Getter;
+
+public class BusinessLogicException extends RuntimeException {
+    @Getter
+    private ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
 }
