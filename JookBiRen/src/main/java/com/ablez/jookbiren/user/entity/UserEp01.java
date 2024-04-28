1c14883c559b2eb78ceb361b85b638184debdcd7
otaeho <dhxogh123123@naver.com>
2024-04-28 20:48:51 +0900

    chore(User): 연관 관계 매핑

--- JookBiRen/src/main/java/com/ablez/jookbiren/user/entity/UserEp01.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/user/entity/UserEp01.java
@@ -1,7 +1,12 @@
 package com.ablez.jookbiren.user.entity;
 
+import com.ablez.jookbiren.quiz.entity.Quiz0Ep01;
+import com.ablez.jookbiren.quiz.entity.Quiz1Ep01;
+import com.ablez.jookbiren.quiz.entity.Quiz2Ep01;
+import com.ablez.jookbiren.quiz.entity.Quiz3Ep01;
 import com.ablez.jookbiren.security.entity.Authority;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -35,6 +40,18 @@ public class UserEp01 {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Authority> authorities = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Quiz0Ep01> quiz0s = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Quiz1Ep01> quiz1s = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Quiz2Ep01> quiz2s = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Quiz3Ep01> quiz3s = new ArrayList<>();
 
     public UserEp01(Long userId, String code) {
         this.userId = userId;
