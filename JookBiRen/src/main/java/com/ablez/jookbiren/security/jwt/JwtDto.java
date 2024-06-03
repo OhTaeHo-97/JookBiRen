231b033883a1ef66eb3d515f243b005b52b1b750
otaeho <dhxogh123123@naver.com>
2024-06-03 23:34:16 +0900

    fix(JwtDto): DTO 형태 변환 - 불필요한 DTO 제거 후 DTO 간략화

--- JookBiRen/src/main/java/com/ablez/jookbiren/security/jwt/JwtDto.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/security/jwt/JwtDto.java
@@ -1,34 +1,17 @@
 package com.ablez.jookbiren.security.jwt;
 
+import com.ablez.jookbiren.security.utils.JwtHeaderUtilEnums;
 import lombok.Getter;
 
 public class JwtDto {
     @Getter
-    public static class AccessTokenDto {
+    public static class TokenDto {
         private String accessToken;
-
-        public AccessTokenDto(String accessToken) {
-            this.accessToken = accessToken;
-        }
-    }
-
-    @Getter
-    public static class RefreshTokenDto {
         private String refreshToken;
 
-        public RefreshTokenDto(String refreshToken) {
-            this.refreshToken = refreshToken;
-        }
-    }
-
-    @Getter
-    public static class TokenDto {
-        private AccessTokenDto accessToken;
-        private RefreshTokenDto refreshToken;
-
         public TokenDto(String accessToken, String refreshToken) {
-            this.accessToken = new AccessTokenDto(accessToken);
-            this.refreshToken = new RefreshTokenDto(refreshToken);
+            this.accessToken = JwtHeaderUtilEnums.GRANT_TYPE.getValue() + accessToken;
+            this.refreshToken = JwtHeaderUtilEnums.GRANT_TYPE.getValue() + refreshToken;
         }
     }
 }
