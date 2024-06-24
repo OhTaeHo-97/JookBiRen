6cfd94ec111444f2bdd6f6903f644405ff74f7c7
otaeho <dhxogh123123@naver.com>
2024-06-24 20:28:29 +0900

    fix(BuyerInfo): Querydsl을 이용한 repository 생성을 위한 기존 JPA repository의 클래스명 변경

--- JookBiRen/src/main/java/com/ablez/jookbiren/buyer/repository/BuyerInfoJpaRepository.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/buyer/repository/BuyerInfoJpaRepository.java
@@ -3,5 +3,5 @@ package com.ablez.jookbiren.buyer.repository;
 import com.ablez.jookbiren.buyer.entity.BuyerInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BuyerInfoRepository extends JpaRepository<BuyerInfo, Long> {
+public interface BuyerInfoJpaRepository extends JpaRepository<BuyerInfo, Long> {
 }
