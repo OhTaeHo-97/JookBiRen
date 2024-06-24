82c0dde297e338016b2aa6534dd04481d5ab3bf1
otaeho <dhxogh123123@naver.com>
2024-06-24 20:29:27 +0900

    feat(BuyerInfo): Querydsl을 이용한 repository 생성, 핸드폰 번호를 이용한 주문자 정보 조회 기능

--- JookBiRen/src/main/java/com/ablez/jookbiren/buyer/repository/BuyerInfoRepository.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/buyer/repository/BuyerInfoRepository.java
@@ -1,4 +1,33 @@
 package com.ablez.jookbiren.buyer.repository;
 
-public class BuyerInfoQuerydslRepository {
+import static com.ablez.jookbiren.buyer.entity.QBuyerInfo.buyerInfo;
+import static com.ablez.jookbiren.order.entity.QOrderInfo.orderInfo;
+
+import com.ablez.jookbiren.buyer.entity.BuyerInfo;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class BuyerInfoRepository {
+    private final EntityManager em;
+    private JPAQueryFactory queryFactory;
+
+    public BuyerInfoRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(this.em);
+    }
+
+    public Optional<BuyerInfo> findByPhone(String phone) {
+        BuyerInfo result = queryFactory.selectFrom(buyerInfo)
+                .innerJoin(buyerInfo.orderInfos, orderInfo)
+                .where(buyerInfo.phone.eq(phone))
+                .distinct()
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
 }
