23a606a3c92349fc36b2baecc5ac97251c6339f7
otaeho <dhxogh123123@naver.com>
2024-07-16 22:11:54 +0900

    fix(Quiz): 중복 로그인 방지를 위해 DB의 토큰과 받은 토큰을 비교하는 로직 추가

--- JookBiRen/src/main/java/com/ablez/jookbiren/quiz/controller/QuizController.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/quiz/controller/QuizController.java
@@ -2,12 +2,12 @@ package com.ablez.jookbiren.quiz.controller;
 
 import com.ablez.jookbiren.dto.JookBiRenDto.Quiz;
 import com.ablez.jookbiren.quiz.service.QuizService;
-import com.ablez.jookbiren.security.interceptor.JwtParseInterceptor;
 import com.ablez.jookbiren.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,24 +19,23 @@ public class QuizController {
     private final UserService userService;
 
     @GetMapping
-    public ResponseEntity getCurrentSituationAndSolvedProblems(int place) {
-        return new ResponseEntity(quizService.getCurrentSituationAndSolvedProblems(place,
-                userService.findByUserId(Long.parseLong(JwtParseInterceptor.getAuthenticatedUsername()))),
+    public ResponseEntity getCurrentSituationAndSolvedProblems(@RequestHeader("Authorization") String accessToken,
+                                                               int place) {
+        return new ResponseEntity(
+                quizService.getCurrentSituationAndSolvedProblems(place, userService.findCurrentUser(accessToken)),
                 HttpStatus.OK);
     }
 
     @GetMapping("/answer")
-    public ResponseEntity checkAlreadySolvedQuiz(String quiz) {
-        return new ResponseEntity(quizService.checkAlreadySolvedQuiz(
-                userService.findByUserId(Long.parseLong(JwtParseInterceptor.getAuthenticatedUsername())),
-                new Quiz(quiz)), HttpStatus.OK);
+    public ResponseEntity checkAlreadySolvedQuiz(@RequestHeader("Authorization") String accessToken, String quiz) {
+        return new ResponseEntity(
+                quizService.checkAlreadySolvedQuiz(userService.findCurrentUser(accessToken), new Quiz(quiz)),
+                HttpStatus.OK);
     }
 
     @GetMapping("/hint")
-    public ResponseEntity getHint(String quiz) {
-        return new ResponseEntity(
-                quizService.findHint(
-                        userService.findByUserId(Long.parseLong(JwtParseInterceptor.getAuthenticatedUsername())),
-                        new Quiz(quiz)), HttpStatus.OK);
+    public ResponseEntity getHint(@RequestHeader("Authorization") String accessToken, String quiz) {
+        return new ResponseEntity(quizService.findHint(userService.findCurrentUser(accessToken), new Quiz(quiz)),
+                HttpStatus.OK);
     }
 }
