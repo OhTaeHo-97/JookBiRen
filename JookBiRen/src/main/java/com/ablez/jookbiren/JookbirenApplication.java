1e8040821e074281900251dce529d7d65830fb91
otaeho <dhxogh123123@naver.com>
2024-10-26 02:03:38 +0900

    fix: 프로젝트, DB 시간대 한국으로 변경

--- JookBiRen/src/main/java/com/ablez/jookbiren/JookbirenApplication.java
+++ JookBiRen/src/main/java/com/ablez/jookbiren/JookbirenApplication.java
@@ -1,10 +1,16 @@
 package com.ablez.jookbiren;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JookbirenApplication {
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JookbirenApplication.class, args);
